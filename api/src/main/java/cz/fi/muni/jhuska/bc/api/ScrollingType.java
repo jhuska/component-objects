1faf2fc9b411a08da08aac2f61a1d3411ec0e202
Juraj Huska <jhuska@redhat.com>
2012-03-07 14:55:32 +0100

    Calendar methods implements

---
+++ api/src/main/java/cz/fi/muni/jhuska/bc/api/ScrollingType.java
@@ -0,0 +1,11 @@
+package cz.fi.muni.jhuska.bc.api;
+
+/**
+ * Enum with different types of scrolling, that is the ways something can be done.
+ * 
+ * @author jhuska
+ */
+public enum ScrollingType {
+    BY_KEYS,
+    BY_MOUSE
+}
---
+++ ftest/src/test/java/cz/fi/muni/jhuska/bc/ftest/components/TestCalendarComponent.java
@@ -0,0 +1,461 @@
+package cz.fi.muni.jhuska.bc.ftest.components;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.bouncycastle.crypto.RuntimeCryptoException;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.joda.time.DateTime;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import cz.fi.muni.jhuska.bc.api.CalendarComponent;
+import cz.fi.muni.jhuska.bc.api.CalendarComponent.CalendarDay;
+import cz.fi.muni.jhuska.bc.api.CalendarComponent.CalendarWeek;
+import cz.fi.muni.jhuska.bc.api.Factory;
+import cz.fi.muni.jhuska.bc.api.ScrollingType;
+import cz.fi.muni.jhuska.bc.ftest.AbstractTest;
+import cz.fi.muni.jhuska.bc.impl.CalendarComponentImpl;
+
+public class TestCalendarComponent extends AbstractTest {
+
+    // @FindBy(xpath = "//td[@class='ecol1']")
+    private CalendarComponent calendar;
+
+    private final By CALENDAR_INPUT = By.className("rf-cal-inp");
+    private final String DATE_FORMAT = "dd/M/yy hh:mm a";
+
+    // @Inject
+    // private Page page;
+
+    private WebElement calendarRoot;
+
+    @Deployment(testable = false)
+    public static WebArchive deploy() {
+        return createDeployment(TestCalendarComponent.class);
+    }
+
+    @BeforeClass
+    public void initCalendar() {
+        calendar = Factory.initializeComponent(CalendarComponentImpl.class);
+    }
+
+    @BeforeMethod
+    public void resetCalendar() {
+        calendarRoot = webDriver.findElement(By.xpath("//*[contains(@id, 'calendar')]"));
+        calendar.setRoot(calendarRoot);
+    }
+
+    @Test
+    public void testShowCalendar() {
+
+        WebElement popup = calendarRoot.findElement(By.className("rf-cal-day-lbl"));
+        assertFalse(popup.isDisplayed(), "The popup should not be displayed yet, since it was not invoked!");
+
+        calendar.showCalendar();
+
+        assertTrue(popup.isDisplayed(), "The popup of the calendar should be displayed!");
+
+        calendar.showCalendar();
+        assertTrue(popup.isDisplayed(),
+            "The popup of the calendar should be displayed also when the showCalendar was invoked on already shown calendar!");
+    }
+
+    @Test
+    public void hideCalendar() {
+
+        WebElement popup = calendarRoot.findElement(By.className("rf-cal-day-lbl"));
+        assertFalse(popup.isDisplayed(), "The popup should not be displayed yet, since it was not invoked!");
+
+        calendar.hideCalendar();
+        assertFalse(popup.isDisplayed(), "The popup should not be displayed yet, since it was not invoked!");
+
+        calendar.showCalendar();
+        assertTrue(popup.isDisplayed(), "The popup of the calendar should be displayed!");
+        calendar.hideCalendar();
+        assertFalse(popup.isDisplayed(), "The popup should not be displayed yet, since it was not invoked!");
+    }
+
+    @Test
+    public void testShowMethodOnInlineCalendar() {
+        calendar.setRoot(webDriver.findElement(By.xpath("//*[contains(@id, 'calendarInline')]")));
+        calendar.showCalendar();
+        // no exception should be thrown
+    }
+
+    @Test
+    public void testHideMethodOnInlineCalendar() {
+        calendar.setRoot(webDriver.findElement(By.xpath("//*[contains(@id, 'calendarInline')]")));
+        try {
+            calendar.hideCalendar();
+            fail("The RuntimeException should be thrown, since invoking hide method on inline calendar!");
+        } catch (RuntimeException ex) {
+            // OK
+        }
+    }
+
+    @Test
+    public void testGetDate() {
+        Date date = calendar.getDate();
+        assertNull(date, "The returned date should be null, since no date was set before getting of the date!");
+
+        date = new Date( System.currentTimeMillis() );
+        calendar.gotoDate(date);
+        Date date1 = calendar.getDate();
+        
+        String inputValue = webDriver.findElement(CALENDAR_INPUT).getAttribute("value");
+        date = getDateFromString(inputValue);
+        assertEquals(date1, date, "The got date should be the same as the one which was set!");
+    }
+
+    @Test
+    public void testGetDateTime() {
+        DateTime dateTime = calendar.getDateTime();
+        assertNull(dateTime,
+            "The returned dateTime(joda time) should be null, since no dateTime was set before getting of the dateTime");
+
+        DateTime dateTime1 = new DateTime(System.currentTimeMillis());
+        calendar.gotoDate(dateTime1);
+
+        String inputValue = webDriver.findElement(CALENDAR_INPUT).getText();
+        dateTime = DateTime.parse(inputValue);
+        assertEquals(dateTime, dateTime1, "The got dateTime(joda time) should be the same as the one which was set!");
+    }
+
+    @Test
+    public void testGotoDate() {
+        Date date1 = new Date(System.currentTimeMillis());
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(date1);
+        cal.roll(Calendar.YEAR, false);
+        cal.roll(Calendar.MONTH, false);
+        date1 = cal.getTime();
+        
+        calendar.gotoDate(date1);
+
+        String inputValue = webDriver.findElement(CALENDAR_INPUT).getAttribute("value");
+        Date date2 = getDateFromString(inputValue);
+        assertEquals(date1.getMonth(), date2.getMonth(), "The got date should be the same as the one which was set!");
+        assertEquals(date1.getYear(), date2.getYear(), "The got date should be the same as the one which was set!");
+    }
+
+    @Test
+    public void testGotoDateTime() {
+        DateTime dateTime1 = new DateTime();
+
+        calendar.gotoDate(dateTime1);
+
+        String inputValue = webDriver.findElement(CALENDAR_INPUT).getText();
+        DateTime dateTime2 = DateTime.parse(inputValue);
+        assertEquals(dateTime1, dateTime2, "The got dateTime should be the same as the one which was set!");
+    }
+
+    @Test
+    public void testGotoDateByMouse() {
+        Date date1 = new Date(System.currentTimeMillis());
+
+        calendar.gotoDate(date1, ScrollingType.BY_MOUSE);
+
+        String inputValue = webDriver.findElement(CALENDAR_INPUT).getText();
+        Date date2 = getDateFromString(inputValue);
+        assertEquals(date1, date2, "The got date should be the same as the one which was set!");
+    }
+
+    @Test
+    public void testGotoDateTimeByMouse() {
+        DateTime dateTime1 = new DateTime();
+
+        calendar.gotoDate(dateTime1, ScrollingType.BY_MOUSE);
+
+        String inputValue = webDriver.findElement(CALENDAR_INPUT).getText();
+        DateTime dateTime2 = DateTime.parse(inputValue);
+        assertEquals(dateTime1, dateTime2, "The got dateTime should be the same as the one which was set!");
+    }
+
+    @Test
+    public void testGotoDateByKeys() {
+
+        try {
+            calendar.gotoDate(new Date(), ScrollingType.BY_KEYS);
+            fail("The IllegalArgumentException should be thrown, since RichFaces calendar does not support "
+                + ScrollingType.BY_KEYS);
+        } catch (IllegalArgumentException ex) {
+            // OK
+        }
+    }
+
+    @Test
+    public void testGotoDateTimeByKeys() {
+
+        try {
+            calendar.gotoDate(new DateTime(), ScrollingType.BY_KEYS);
+            fail("The IllegalArgumentException should be thrown, since RichFaces calendar does not support "
+                + ScrollingType.BY_KEYS);
+        } catch (IllegalArgumentException ex) {
+            // OK
+        }
+    }
+
+    @Test
+    public void testGotoNextDay() {
+        checkGotoNextMethods(java.util.Calendar.DAY_OF_MONTH);
+    }
+
+    @Test
+    public void testGotoNextMonth() {
+        checkGotoNextMethods(java.util.Calendar.MONTH);
+    }
+
+    @Test
+    public void testGotoNextYear() {
+        checkGotoNextMethods(java.util.Calendar.YEAR);
+    }
+
+    @Test
+    public void testGotoPreviousDay() {
+        checkGotoPreviousMethods(Calendar.DAY_OF_MONTH);
+    }
+
+    @Test
+    public void testGotoPreviousMonth() {
+        checkGotoPreviousMethods(Calendar.MONTH);
+    }
+
+    @Test
+    public void testGotoPreviousYear() {
+        checkGotoPreviousMethods(Calendar.YEAR);
+    }
+
+    @Test
+    public void testGetDayOfMonth() {
+        CalendarDay day = calendar.getDayOfMonth();
+        assertNotNull(day, "When no date is set, the returned day should has value of the current date's day!");
+
+        Date date1 = new Date(System.currentTimeMillis());
+        Calendar cal1 = new GregorianCalendar();
+        cal1.setTime(date1);
+
+        assertEquals(day.toInt(), cal1.get(Calendar.DAY_OF_MONTH), "The returned day should be set to the current date's day!");
+
+        int expectedDay = 10;
+        cal1.set(2012, 2, expectedDay);
+        calendar.gotoDate(cal1.getTime());
+        day = calendar.getDayOfMonth();
+
+        assertEquals(day.toInt(), expectedDay, "The returned day is wrong!");
+    }
+
+    @Test
+    public void testGetSpecificDayOfMonth() {
+        int expectedDay = 1;
+        CalendarDay day = calendar.getDayOfMonth(expectedDay);
+        assertEquals(day.toInt(), expectedDay, "Returned day is wrong!");
+    }
+
+    @Test
+    public void testGetDaysOfMonth() {
+        List<CalendarDay> days = calendar.getDaysOfMonth();
+        Calendar cal1 = new GregorianCalendar();
+        cal1.setTime(new Date(System.currentTimeMillis()));
+
+        assertNotNull(days, "It should not return null, no date was set so it should return all days of current date's month");
+        assertTrue(days.size() > 0, "The number of days should be bigger than 0");
+        assertEquals(days.size(), cal1.getActualMaximum(Calendar.DAY_OF_MONTH));
+
+        cal1.set(2012, 2, 1);
+        calendar.gotoDate(cal1.getTime());
+
+        days = calendar.getDaysOfMonth();
+        assertNotNull(days, "It should not return null, it should return number of days of the month in date " + cal1.getTime());
+        assertTrue(days.size() > 0, "The number of days should be bigger than 0");
+        assertEquals(days.size(), cal1.getActualMaximum(Calendar.DAY_OF_MONTH));
+    }
+
+    @Test
+    public void testGetWeekOfYear() {
+        CalendarWeek week = calendar.getWeekOfYear();
+        assertNotNull(week, "It should return the week of the current date, as no date was set!");
+
+        Date date1 = new Date(System.currentTimeMillis());
+        Calendar cal1 = new GregorianCalendar();
+        cal1.setTime(date1);
+
+        assertEquals(week.toInt(), cal1.get(Calendar.WEEK_OF_YEAR),
+            "The returned week should be set to the current date's week of the year!");
+
+        int expectedWeek = 1;
+        cal1.set(2012, 1, expectedWeek);
+        calendar.gotoDate(cal1.getTime());
+        week = calendar.getWeekOfYear();
+
+        assertEquals(week.toInt(), expectedWeek, "The returned week is wrong!");
+    }
+
+    @Test
+    public void testGetSpecificWeekOfYear() {
+        CalendarWeek week = calendar.getWeekOfYear(1);
+        assertNotNull(week, "The returned week should not be null, it should return the week of the current date!");
+
+        Calendar cal1 = new GregorianCalendar();
+        cal1.setTime(new Date(System.currentTimeMillis()));
+
+        assertEquals(week.whichYear(), cal1.get(Calendar.YEAR), "The week is not a part of the correct year!");
+
+        int expectedYear = 2011;
+        cal1.set(expectedYear, 1, 1);
+        calendar.gotoDate(cal1.getTime());
+        week = calendar.getWeekOfYear(1);
+
+        assertEquals(week.whichYear(), expectedYear, "The returned week is not a part of the correct year!");
+    }
+
+    @Test
+    public void testGetWeeksOfYear() {
+        List<CalendarWeek> weeks = calendar.getWeeksOfYear();
+        Calendar cal1 = new GregorianCalendar();
+        cal1.setTime(new Date(System.currentTimeMillis()));
+
+        assertNotNull(weeks,
+            "It should not return null, no date was set so it should return all weeks of the current date's month");
+        assertTrue(weeks.size() > 0, "The number of days should be bigger than 0");
+        assertEquals(weeks.size(), cal1.getActualMaximum(Calendar.WEEK_OF_YEAR));
+
+        cal1.set(2012, 2, 1);
+        calendar.gotoDate(cal1.getTime());
+
+        weeks = calendar.getWeeksOfYear();
+        assertNotNull(weeks,
+            "It should not return null, it should return number of days of the month in date " + cal1.getTime());
+        assertTrue(weeks.size() > 0, "The number of days should be bigger than 0");
+        assertEquals(weeks.size(), cal1.getActualMaximum(Calendar.WEEK_OF_YEAR));
+    }
+
+    // Help Methods //////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Checks methods for shifting date to the past
+     * 
+     * @param whatIsShifted can has these values: <code>java.util.Calendar.DAY_OF_MONTH</code>,
+     *        <code>java.util.Calendar.MONTH</code>, <code>java.util.Calendar.YEAR</code>
+     */
+    private void checkGotoPreviousMethods(int whatIsShifted) {
+        Date date1 = new Date(System.currentTimeMillis());
+        calendar.gotoDate(date1);
+        java.util.Calendar cal1 = new GregorianCalendar();
+        cal1.setTime(date1);
+
+        switch (whatIsShifted) {
+            case java.util.Calendar.DAY_OF_MONTH:
+                calendar.gotoPreviousDay();
+                break;
+            case java.util.Calendar.MONTH:
+                calendar.gotoPreviousMonth();
+                break;
+            case java.util.Calendar.YEAR:
+                calendar.gotoPreviousYear();
+                break;
+            default:
+                throw new IllegalArgumentException("Wrong value of what will be shifted, it is not a day or month or year");
+        }
+
+        Date date2 = calendar.getDate();
+        java.util.Calendar cal2 = new GregorianCalendar();
+        cal2.setTime(date2);
+
+        assertTrue(cal1.after(cal2), "It was not shifted to the past!");
+        switch (whatIsShifted) {
+            case java.util.Calendar.DAY_OF_MONTH:
+                assertTrue(cal1.get(java.util.Calendar.DAY_OF_MONTH) == (cal2.get(java.util.Calendar.DAY_OF_MONTH) + 1),
+                    "It should be shifted by one day to past!");
+            case java.util.Calendar.MONTH:
+                assertTrue(cal1.get(java.util.Calendar.MONTH) == (cal2.get(java.util.Calendar.MONTH) + 1),
+                    "It should be shifted by one month to past!");
+            case java.util.Calendar.YEAR:
+                assertTrue(cal1.get(java.util.Calendar.YEAR) == (cal2.get(java.util.Calendar.YEAR) + 1),
+                    "It should be shifted by one year to past!");
+        }
+    }
+
+    /**
+     * Checks methods for shifting date to the future
+     * 
+     * @param whatIsShifted can has these values: <code>java.util.Calendar.DAY_OF_MONTH</code>,
+     *        <code>java.util.Calendar.MONTH</code>, <code>java.util.Calendar.YEAR</code>
+     */
+    private void checkGotoNextMethods(int whatIsShifted) {
+
+        Date date1 = new Date(System.currentTimeMillis());
+        calendar.gotoDate(date1);
+        java.util.Calendar cal1 = new GregorianCalendar();
+        cal1.setTime(date1);
+
+        switch (whatIsShifted) {
+            case java.util.Calendar.DAY_OF_MONTH:
+                calendar.gotoNextDay();
+                break;
+            case java.util.Calendar.MONTH:
+                calendar.gotoNextMonth();
+                break;
+            case java.util.Calendar.YEAR:
+                calendar.gotoNextYear();
+                break;
+            default:
+                throw new IllegalArgumentException("Wrong value of what will be shifted, it is not a day or month or year");
+        }
+
+        Date date2 = calendar.getDate();
+        java.util.Calendar cal2 = new GregorianCalendar();
+        cal2.setTime(date2);
+
+        assertTrue(cal1.before(cal2), "It was not shifted to the future!");
+        switch (whatIsShifted) {
+            case java.util.Calendar.DAY_OF_MONTH:
+                assertTrue(cal1.get(java.util.Calendar.DAY_OF_MONTH) == (cal2.get(java.util.Calendar.DAY_OF_MONTH) - 1),
+                    "It should be shifted by one day to future!");
+            case java.util.Calendar.MONTH:
+                assertTrue(cal1.get(java.util.Calendar.MONTH) == (cal2.get(java.util.Calendar.MONTH) - 1),
+                    "It should be shifted by one month to future!");
+            case java.util.Calendar.YEAR:
+                assertTrue(cal1.get(java.util.Calendar.YEAR) == (cal2.get(java.util.Calendar.YEAR) - 1),
+                    "It should be shifted by one year to future!");
+        }
+    }
+
+    /**
+     * Parse the <code>string</code> value and returns the Date, the date is in format dd/M/yy hh:mm a
+     * 
+     * @param string
+     * @return
+     * @throws ParseException
+     */
+    private Date getDateFromString(String string) {
+        if (string.trim().length() == 0) {
+            throw new RuntimeException("You want get a date from an empty string!");
+        }
+        SimpleDateFormat format = new SimpleDateFormat(DATE_FORMAT);
+
+        Date date;
+        try {
+            date = format.parse(string);
+        } catch (ParseException e) {
+            throw new RuntimeException("error while parsing the date!");
+        }
+
+        return date;
+    }
+}
