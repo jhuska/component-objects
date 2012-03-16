940a99d97a6ac6030fd77e2d6c5303be261dde58
Juraj Huska <gooseka2@gmail.com>
2012-03-17 00:01:05 +0100

    initial methods added

--- api/src/main/java/cz/fi/muni/jhuska/bc/api/ValidationComponent.java
+++ api/src/main/java/cz/fi/muni/jhuska/bc/api/ValidationComponent.java
@@ -1,5 +1,70 @@
 package cz.fi.muni.jhuska.bc.api;
 
+import java.util.List;
+
 public interface ValidationComponent extends Component {
 
+	/**
+	 * Returns the list containing all the messages currently on the page. 
+	 * 
+	 * @return
+	 */
+	List<Message> getAllMessages();
+	
+	/**
+	 * Returns the list containing all the error messages currently on the page.
+	 * 
+	 * @return
+	 */
+	List<ErrorMessage> getAllErrorMessages();
+	
+	/**
+	 * Returns the list containing all the info messages currently on the page.
+	 * 
+	 * @return
+	 */
+	List<ErrorMessage> getAllInfoMessages();
+	
+	/**
+	 * Determines whether the given message is rendered on the page.
+	 * 
+	 * @param msg
+	 * @return
+	 */
+	boolean isMessageRendered(Message msg);
+	
+	/**
+	 * Represents the general messages which can appear on the page during validation.
+	 * 
+	 * @author jhuska
+	 *
+	 */
+	public interface Message {
+		
+		/**
+		 * Returns the string representation of this message.
+		 * @return
+		 */
+		String getValue();
+	}
+	
+	/**
+	 * Represents error message which can appear on the page during validation.
+	 * 
+	 * @author jhuska
+	 *
+	 */
+	public interface ErrorMessage extends Message {
+		
+	}
+	
+	/**
+	 * Represents info message which can appear on the page during validation.
+	 * 
+	 * @author jhuska
+	 *
+	 */
+	public interface InfoMessage extends Message {
+		
+	}
 }
