01ab19140659a2d402d2e29bea5e109c8609820c
Juraj Huska <gooseka2@gmail.com>
2012-03-22 22:53:06 +0100

    initial support for arquillian extension

---
+++ drone/src/main/java/org/jboss/arquillian/drone/configuration/ComponentModelDroneConfiguration.java
@@ -0,0 +1,5 @@
+package org.jboss.arquillian.drone.configuration;
+
+public class ComponentModelDroneConfiguration {
+
+}
---
+++ drone/src/main/java/org/jboss/arquillian/drone/impl/ComponentModelWebDriverSupport.java
@@ -0,0 +1,5 @@
+package org.jboss.arquillian.drone.impl;
+
+public class ComponentModelWebDriverSupport {
+
+}
