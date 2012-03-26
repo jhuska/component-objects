bafba0e52ecfd280429a0c59680b10984c8fb71c
Juraj Huska <jhuska@redhat.com>
2012-03-26 22:12:40 +0200

    static part of the message added

--- api/src/main/java/cz/fi/muni/jhuska/bc/api/ValidationComponent.java
+++ api/src/main/java/cz/fi/muni/jhuska/bc/api/ValidationComponent.java
@@ -3,74 +3,95 @@ package cz.fi.muni.jhuska.bc.api;
 import java.util.List;
 
 /**
- * Validation component represents all kind of validation messages mostly.
+ * Validation component represents set of inputs which are validated somehow. That is, there are some constraints, which have to
+ * be fulfilled in order to be submitted, like various patterns etc.
  * 
  * @author jhuska
- *
+ * 
  */
 public interface ValidationComponent extends Component {
 
-	/**
-	 * Returns the list containing all the messages currently on the page. 
-	 * 
-	 * @return
-	 */
-	List<Message> getAllMessages();
-	
-	/**
-	 * Returns the list containing all the error messages currently on the page.
-	 * 
-	 * @return
-	 */
-	List<ErrorMessage> getAllErrorMessages();
-	
-	/**
-	 * Returns the list containing all the info messages currently on the page.
-	 * 
-	 * @return
-	 */
-	List<ErrorMessage> getAllInfoMessages();
-	
-	/**
-	 * Determines whether the given message is rendered on the page.
-	 * 
-	 * @param msg
-	 * @return
-	 */
-	boolean isMessageRendered(Message msg);
-	
-	/**
-	 * Represents the general messages which can appear on the page during validation.
-	 * 
-	 * @author jhuska
-	 *
-	 */
-	public interface Message {
-		
-		/**
-		 * Returns the string representation of this message.
-		 * @return
-		 */
-		String getValue();
-	}
-	
-	/**
-	 * Represents error message which can appear on the page during validation.
-	 * 
-	 * @author jhuska
-	 *
-	 */
-	public interface ErrorMessage extends Message {
-		
-	}
-	
-	/**
-	 * Represents info message which can appear on the page during validation.
-	 * 
-	 * @author jhuska
-	 *
-	 */
-	public interface InfoMessage extends Message {
-		
-	}
+    /**
+     * Returns the list containing all the messages currently on the page.
+     * 
+     * @return
+     */
+    List<Message> getAllMessages();
+
+    /**
+     * Returns the list containing all the error messages currently on the page.
+     * 
+     * @return
+     */
+    List<ErrorMessage> getAllErrorMessages();
+
+    /**
+     * Returns the list containing all the info messages currently on the page.
+     * 
+     * @return
+     */
+    List<ErrorMessage> getAllInfoMessages();
+
+    /**
+     * Determines whether the given message is rendered on the page.
+     * 
+     * @param msg
+     * @return
+     */
+    boolean isMessageRendered(Message msg);
+
+    /**
+     * Determines whether the given static part of the message is rendered on the page.
+     * 
+     * @param msg
+     * @return
+     */
+    boolean isMessageRendered(StaticMessagePart msg);
+
+    /**
+     * Represents the general messages which can appear on the page during validation.
+     * 
+     * @author jhuska
+     * 
+     */
+    public interface Message {
+
+        StaticMessagePart getStaticMessagePart();
+
+        VariableMessagePart getVariableMessagePart();
+
+        void setVariableMessagePart();
+
+        void setStaticMessagePart();
+    }
+
+    /**
+     * Represents error message which can appear on the page during validation.
+     * 
+     * @author jhuska
+     * 
+     */
+    public interface ErrorMessage extends Message {
+
+    }
+
+    /**
+     * Represents info message which can appear on the page during validation.
+     * 
+     * @author jhuska
+     * 
+     */
+    public interface InfoMessage extends Message {
+
+    }
+
+    public interface StaticMessagePart {
+
+        String getValue();
+    }
+
+    public interface VariableMessagePart {
+
+        String getValue();
+    }
 }
