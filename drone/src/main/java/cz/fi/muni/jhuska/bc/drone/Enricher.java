deec0c4309af6084a331ad6af2275dda2dafc751
Juraj Huska <gooseka2@gmail.com>
2012-03-31 17:46:44 +0200

    added support for setting the declared root to initialised component

--- drone/src/main/java/cz/fi/muni/jhuska/bc/drone/Enricher.java
+++ drone/src/main/java/cz/fi/muni/jhuska/bc/drone/Enricher.java
@@ -1,35 +1,67 @@
 package cz.fi.muni.jhuska.bc.drone;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Iterator;
 import java.util.List;
 
-import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.context.GrapheneContext;
 import org.jboss.arquillian.test.spi.TestEnricher;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
 
-import cz.fi.muni.jhuska.bc.annotations.Component;
 import cz.fi.muni.jhuska.bc.api.AbstractComponent;
 import cz.fi.muni.jhuska.bc.api.Factory;
 
 public class Enricher implements TestEnricher {
 
-	private static final String COMPONENT_ANNOTATION = "cz.fi.muni.jhuska.bc.annotations.Component";
+	private static final String COMPONENT_ANNOTATION = "org.openqa.selenium.support.FindBy";
 
 	@Override
 	public void enrich(Object testCase) {
 		if (ReflectionHelper.isClassPresent(COMPONENT_ANNOTATION)) {
 
+			// gets all fields and then removes these which does not extend
+			// particular class according to component contract
 			List<Field> fields = ReflectionHelper.getFieldsWithAnnotation(
-					testCase.getClass(), Component.class);
+					testCase.getClass(), FindBy.class);
+			fields = removeNotFindByComponents(fields);
 
 			for (Field componentField : fields) {
 
+				// initialise component
 				Class implementationClass = componentField.getType();
 				Object component = Factory
 						.initializeComponent(implementationClass);
 
+				// sets the root of the component, retrieved from annotation
+				FindBy findBy = componentField.getAnnotation(FindBy.class);
+				final By by = Factory.getReferencedBy(findBy);
+
+				WebElement rootElement = (WebElement) Proxy.newProxyInstance(
+						WebElement.class.getClassLoader(),
+						new Class<?>[] { WebElement.class },
+						new InvocationHandler() {
+
+							@Override
+							public Object invoke(Object proxy, Method method,
+									Object[] args) throws Throwable {
+								
+								WebDriver driver = GrapheneContext.get();
+								WebElement root =  driver.findElement(by);
+								
+								return (Object) method.invoke(root, args);
+							}
+						});
+				
+				((AbstractComponent) component).setRoot(rootElement); 
+
+				// sets the component to the test class object
 				boolean accessible = componentField.isAccessible();
 				if (!accessible) {
 					componentField.setAccessible(true);
@@ -46,6 +78,45 @@ public class Enricher implements TestEnricher {
 				}
 			}
 		}
+	}
+
+	/**
+	 * It removes all components from given list which does not extend the
+	 * <code>cz.fi.muni.jhuska.bc.api.AbstractComponent</code>
+	 * 
+	 * @param findByFields
+	 * @return
+	 */
+	private List<Field> removeNotFindByComponents(List<Field> findByFields) {
+
+		for (Iterator<Field> i = findByFields.iterator(); i.hasNext();) {
+
+			try {
+				Field field = i.next();
+
+				Class clazz = Class.forName(field.getGenericType().toString()
+						.split(" ")[1]);
+
+				String superClass = null;
+				try {
+					superClass = (clazz.getGenericSuperclass().toString()
+							.split(" ")[1]);
+				} catch (NullPointerException ex) {
+					// it is ok in some cases, lets continue with other elements
+				}
+
+				if (superClass == null
+						|| !superClass
+								.equals("cz.fi.muni.jhuska.bc.api.AbstractComponent")) {
+
+					i.remove();
+				}
+			} catch (ClassNotFoundException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		return findByFields;
 	}
 
 	@Override
--- drone/src/test/java/cz/fi/muni/jhuska/bc/drone/test/TestInitializingComponent.java
+++ drone/src/test/java/cz/fi/muni/jhuska/bc/drone/test/TestInitializingComponent.java
@@ -1,31 +1,69 @@
 package cz.fi.muni.jhuska.bc.drone.test;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
+import java.io.File;
+import java.net.URL;
+
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.context.GrapheneContext;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.ArchivePaths;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import cz.fi.muni.jhuska.bc.annotations.Component;
 import cz.fi.muni.jhuska.bc.api.AbstractComponentStub;
-import cz.fi.muni.jhuska.bc.drone.Enricher;
 
 public class TestInitializingComponent extends Arquillian {
 
-	@Component//mozno nahradit finby
-	AbstractComponentStub abstractComponent;//radsej stub pozret
+	@FindBy(xpath = "//div[@id='rootElement']")
+	AbstractComponentStub abstractComponent;
+	
+	@FindBy(xpath = "//div[@id='rootElement']")
+	WebElement element;
+
+	private final String EXPECTED_NESTED_ELEMENT_TEXT = "Some Value";
+
+	@Drone
+	WebDriver driver;
+
+	@ArquillianResource
+	protected URL contextRoot;
+
+	private static final String WEB_APP_SRC = "src/test/webapp";
 
 	@Deployment(testable = false)
 	public static WebArchive deploy() {
-		return ShrinkWrap.create(WebArchive.class).addClass(Enricher.class);
+		return ShrinkWrap.create(WebArchive.class, "drone-test.war")
+				.addAsWebResource(new File(WEB_APP_SRC + "/index.html"),
+						ArchivePaths.create("index.html"));
+	}
+	
+	@BeforeMethod
+	public void initGrapheneContext() {
+		GrapheneContext.set(driver);
 	}
 
 	@Test
 	public void testComponentIsInitialized() {
 		assertNotNull(abstractComponent,
 				"AbstractComponent should be initialised at this point!");
+	}
+
+	@Test
+	public void testComponentHasSetRootCorrectly() {
+		driver.get(contextRoot + "index.html");
+
+		assertEquals(abstractComponent.invokeMethodOnElementRefByXpath(),
+				EXPECTED_NESTED_ELEMENT_TEXT, "The root was not set correctly!");
 	}
 
 }
